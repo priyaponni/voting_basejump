c53b49e0db61ab7d6254ee1e9b87bf15bcce7865
Ponni Priyadharshni <priyaponni@gmail.com>
2015-12-19 00:03:04 +0000

    .env changes

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,9 +1,5 @@
 'use strict';
 
 module.exports = {
-	'githubAuth': {
-		'clientID': process.env.GITHUB_KEY,
-		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
-	}
+	
 };
