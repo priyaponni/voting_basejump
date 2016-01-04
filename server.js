139e7be6993b046abc2619171e0ca05a26fa614a
Ponni Priyadharshni <priyaponni@gmail.com>
2016-01-04 23:35:12 +0000

    Adding Mongolab URI

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var sessions = require('client-sessions');
 var app = express();
 require('dotenv').load();
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URI);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
